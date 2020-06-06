17a79e088b51f83585d39cbcedbdc00b49c35df9
kelvindominguez1595 <kelvin.dominguez95@gmail.com>
2020-06-06 17:10:17 -0600

    Crear migración de tabla productos

---
+++ database/migrations/2020_06_06_170532_create_productos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->string('producto');
+            $table->integer('cantidad',0);
+            $table->double('precio_compra', 8,2);
+            $table->double('precio_venta', 8,2);
+            $table->integer('categorias_id');
+            $table->foreign('categorias_id')->references('id')->on('categorias');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+}
