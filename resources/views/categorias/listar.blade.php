42dd4e79c9a81905c73813751a00e14dd6dc7dea
kelvindominguez1595 <kelvin.dominguez95@gmail.com>
2020-06-12 14:57:06 -0600

    Creación de vista listar categorias

---
+++ resources/views/categorias/listar.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="container-fluid">
+        <h1 class="mt-4">Categorias</h1>
+        <ol class="breadcrumb mb-4">
+            <li class="breadcrumb-item active">Categorias</li>
+        </ol>
+        <div class="card mb-4">
+            <div class="card-header"> <i class="fa fa-cubes mr-1"></i>Categorias</div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <td>Categoria</td>
+                                <td>Opciones</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
