19bce5205ec009e6acc5119bcf81db32a2f79b80
kelvindominguez1595 <kelvin.dominguez95@gmail.com>
2020-06-12 14:39:35 -0600

    Configuración de de modelo Productos

--- app/Productos.php
+++ app/Productos.php
@@ -3,8 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Productos extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = [
+        'producto', 'cantidad', 'precio_compra', 'precio_venta', 'categorias_id',
+    ];
 }
