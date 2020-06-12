a28cfe1b0344fdda20752b39f884c2511d72468a
kelvindominguez1595 <kelvin.dominguez95@gmail.com>
2020-06-12 16:52:22 -0600

    Agrego el method PUT en el formulario para poder editar

--- resources/views/categorias/editar.blade.php
+++ resources/views/categorias/editar.blade.php
@@ -9,7 +9,9 @@
         <div class="card-body">
             <form class="form-horizontal" action="{{ route('categorias.update', $datos->id) }}" method="POST">
                 @csrf
-                
+                {{-- Para poder actualizar debemos de pasar el metodo put aqui siempre
+                dejando el POST en la etiqueta form --}}
+              @method('PUT')
                 <div class="form-group">
                     <label for="categoria">Categoria</label>
                     <input type="text" name="categoria" class="form-control" placeholder="Categoria" value="{{ $datos->categoria }}">
